3506b7a26f0b8fa9df33944d91fe14d145c97444
t-p-r <quanthangthien1000@gmail.com>
2025-02-21 23:01:16 -0800

    remove partial_sum

--- src/base/radix_sort_base.hpp
+++ src/base/radix_sort_base.hpp
@@ -42,7 +42,9 @@ inline void __do_bucket_sort(IteratorSource source_first,
     // TODO: replace with SIMD
     for (auto it = source_first; it != source_last; ++it)
         bucket[radix_func(*it)]++;
-    std::partial_sum(bucket.begin(), bucket.end(), bucket.begin());
+    for (std::size_t i = 1; i < _Bucket_size; ++i) {
+        bucket[i] += bucket[i - 1];
+    }
     for (auto it = source_last; --it != source_first - 1;)  // black magic
         dest_first[--bucket[radix_func(*it)]] = *it;
 }
