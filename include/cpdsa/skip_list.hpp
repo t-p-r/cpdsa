1b6d85e3d3107a3a72a1129e8fb77871b1d53b96
t-p-r <quanthangthien1000@gmail.com>
2026-04-03 12:59:46 -0700

    add skip list stub

--- include/cpdsa/skip_list.hpp
+++ include/cpdsa/skip_list.hpp
@@ -7,6 +7,27 @@
 #ifndef CPDSA_SKIP_LIST
 #define CPDSA_SKIP_LIST
 
-namespace cpdsa {}  // namespace cpdsa
+namespace cpdsa {
+
+/**
+ * @brief Types eligible to be elements of @c skip_list.
+ */
+#if __cplusplus >= 202002L
+template <typename _Tp>
+concept Skip_list_element_type = requires(_Tp a, _Tp b) { a < b; };
+#else
+// shit goes here
+#endif
+
+/**
+ * @brief A container maintaining elements in increasing order
+ *
+ */
+template<Skip_list_element_type _Tp>
+class skip_list {
+
+};
+
+}  // namespace cpdsa
 
 #endif /* CPDSA_SKIP_LIST */
